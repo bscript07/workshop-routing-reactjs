8e344fbc3f9902e523e0cdaa072ff4c5ea5ac606
Brayan Markov <brayan_markov@yahoo.com>
2024-07-08 16:34:13 +0300

    Add catalogue page add register page add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Header from "./components/header/Header"
+import Home from "./components/home/Home"
+import Catalogue from './components/catalogue/Catalogue';
+import Create from './components/create/Create';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
+
 function App() {
 
   return (
-    <>
-    <h1>Hello world!</h1>
-    </>
+    <div id="box">
+    <Header />
+
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/catalogue' element={<Catalogue />}/>
+      <Route path='/register' element={<Register />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/create' element={<Create />}/>
+    </Routes>
+    </div>
   )
 }
 
